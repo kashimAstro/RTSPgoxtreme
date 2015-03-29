aba6407426b2518a270c2e3caf9677db61bd454f
kashimAstro <d.longobardi@ziggurats.net>
2015-03-29 19:35:54 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -30,19 +30,42 @@ class xApp : public ofBaseApp {
 			p.draw(0,0);
 		}
 
+		string img;
 		void keyPressed(int key){
+			if(key == 'g'){
+				cout<<"width:"<<p.getWidth()<<",height:"<<p.getHeight()<<endl;
+				cout<<"path:"<<p.getMoviePath()<<endl;
+			        cout<<"position:"<<p.getPosition()<<endl;
+				cout<<"speed:"<<p.getSpeed()<<endl;
+               			cout<<"duration:"<<p.getDuration()<<endl;
+             			cout<<"movieDone"<<p.getIsMovieDone()<<endl;
+			}
 			if(key == 'l'){
 				vector<string> s = rtsp.list(4);
 				for(int i = 0; i < s.size(); i++) {
 					cout << s[i] <<endl;
 				}
+				if(s.size()>0){
+					img=s[3];
+					cout<<"select img:"<<img<<endl;
+				}
 			}
+			if(key == 'd'){
+				vector<string> fileName = ofSplitString(img," ");
+				string localFolder = "/var/tmp/";
+				string remoteFolder= rtsp.getPath(4);
+				cout<<"file-name:"<<fileName[fileName.size()-1]<<",local-folder:"<<localFolder<<",remote-folder:"<<remoteFolder<<endl;
+				if(rtsp.getFile( fileName[fileName.size()-1], localFolder, remoteFolder )){
+					cout<<"download: " <<fileName[fileName.size()-1]<<localFolder<<endl;
+				}
+			}
+			
 		}
 };
 
 int main( ){
 	ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1280, 720, OF_WINDOW);
+	ofSetupOpenGL(&window, 640, 360, OF_WINDOW);
 	ofRunApp(new xApp());
 
 }
--- src/rtsp.h
+++ src/rtsp.h
@@ -3,50 +3,51 @@
 
 class RTSPXtreme {
 	private:
-	string host;
-	string user;
-	string pass;
-	vector<string> remote_path;
+        string host;
+        string user;
+        string pass;
+        vector<string> remote_path;
 
-        public:
+    public:
         FTP client;
-	
-	RTSPXtreme() {
-		host="192.168.1.1";
-		user="1234567890";
-		pass="1234567890";
 
-		client.setup(host,user,pass);
-		client.setVerbose(true);
+        RTSPXtreme() {
+            host="192.168.1.1";
+            user="1234567890";
+            pass="1234567890";
 
-		remote_path.push_back("/");             //0
-		remote_path.push_back("/DCIM/");	//1
-		remote_path.push_back("/DCIM/100CARDV/");//2
-		remote_path.push_back("/DCIM/100EVENT/");//3
-		remote_path.push_back("/DCIM/100IMAGE/");   //4
-		remote_path.push_back("/DCIM/100VIDEO/");   //5
-		cout<<"init"<<endl;
-	}
+            client.setup(host,user,pass);
+            client.setVerbose(true);
 
-	~RTSPXtreme(){
+            remote_path.push_back("/");              //0
+            remote_path.push_back("/DCIM/");	 //1
+            remote_path.push_back("/DCIM/100CARDV/");//2
+            remote_path.push_back("/DCIM/100EVENT/");//3
+            remote_path.push_back("/DCIM/100IMAGE/");//4
+            remote_path.push_back("/DCIM/100VIDEO/");//5
+        }
 
-	}
+        ~RTSPXtreme(){
 
-	vector<string> list(int path){
-		cout<<"path:"<<remote_path[path]<<endl;
-		return client.list(remote_path[path]);
-	}
+        }
 
-	string getCameraTrack1() {
-		string buffer;
-		buffer = "rtsp://192.168.1.1/MJPG/track1";
-		//rtsp://192.168.1.1/MJPG/track1;seq=25838;rtptime=100001581
-		return buffer;
-	}
+        string getPath(int path){
+            return remote_path[path];
+        }
 
-        string getCameraMediaPath( int width=640, int height=360, int quality=80, int bitrate=0x2dc6c0 ) {
+        vector<string> list(int path){
+            cout<<"path:"<<remote_path[path]<<endl;
+            return client.list(remote_path[path]);
+        }
+
+        string getCameraTrack1() {
+            string buffer = "rtsp://"+getHost()+"/MJPG/track1";//;seq=25838;rtptime=100001581
+            return buffer;
+        }
+
+        string getCameraMediaPath( int width=320, int height=240, int quality=80, int bitrate=8000000/*0x2dc6c0*/ ) {
                 string buffer;
-                buffer = "rtsp://192.168.1.1/MJPG?W=";
+                buffer = "rtsp://"+getHost()+"/MJPG?W=";
                 buffer += ofToString(width);
                 buffer += ofToString("&H=");
                 buffer += ofToString(height);
@@ -58,43 +59,46 @@ class RTSPXtreme {
         }
 
         void takePhoto(){
-
+		//
+		//
         }
 
         void recordingVideo(){
-
+		//
+		//
         }
 
-        void off(){
-
+        void offCamera(){
+		//
+		//
         }
 
-	bool sendFile(string _path, string remotepath) {
-		ofFile file(_path);
-		string path = file.path().substr(0,file.path().length()-file.getFileName().length());
-		if(client.send(file.getFileName(),path,"/")>0){
-			return true;
-		}
-		return false;
-	}
+        bool sendFile(string _path, string remotepath) {
+            ofFile file(_path);
+            string path = file.path().substr(0,file.path().length()-file.getFileName().length());
+            if(client.send(file.getFileName(),path,"/")>0){
+                return true;
+            }
+            return false;
+        }
 
-	bool getFile(string name, string path, int remotepath){	
-		if(client.get(name,path,remote_path[remotepath])>0){
-		       return true;
-		}
-		return false;
-	}
+        bool getFile(string name, string path, string  remotepath){
+            if(client.get(name,path,remotepath)>0){
+                   return true;
+            }
+            return false;
+        }
 
-	string getHost(){
-		return host;
-	}
+        string getHost(){
+            return host;
+        }
 
-	string getUser(){
-		return user;
-	}
+        string getUser(){
+            return user;
+        }
 
-	string getPass(){
-		return pass;
-	}
+        string getPass(){
+            return pass;
+        }
 
 };
